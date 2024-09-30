fb130cfaec933b1c321914688bb13a8c79f5eb14
Igor Botukhov <igor.botukhov@gmail.com>
2024-09-30 09:53:54 +0000

    Added enums

---
+++ main.rb
@@ -0,0 +1,20 @@
+#encoding:utf-8
+
+module game_character
+    PLAYER  = :player
+    MONSTER = :monster
+end
+
+module directions:
+    LEFT  = :left
+    RIGHT = :right
+    UP    = :up
+    DOWN  = :down
+end
+
+module orientation:
+    VERTICAL   = :vertical
+    HORIZONTAL = :horizontal
+end
+
+
