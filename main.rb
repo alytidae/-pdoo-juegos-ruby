45a78f73af97360cc383ee59b83d9da041c28af1
Igor Botukhov <igor.botukhov@gmail.com>
2024-09-30 10:04:17 +0000

    Changed names and created Weapon class

--- main.rb
+++ main.rb
@@ -1,20 +1,38 @@
-#encoding:utf-8
-
-module game_character
+module GameCharacter
     PLAYER  = :player
     MONSTER = :monster
 end
 
-module directions:
+module Directions
     LEFT  = :left
     RIGHT = :right
     UP    = :up
     DOWN  = :down
 end
 
-module orientation:
+module Orientation
     VERTICAL   = :vertical
     HORIZONTAL = :horizontal
 end
 
+class Weapon
+    def initialize(power, uses)  
+        @power = power
+        @uses = uses
+    end
 
+    def attack
+        if @uses > 0
+            @uses -= 1
+            return @power
+        end
+        return 0
+    end
+
+    def to_s
+        "W[#{@power}, #{@uses}]"
+    end
+end
+
+w = Weapon.new(10, 5)
+puts w.to_s
