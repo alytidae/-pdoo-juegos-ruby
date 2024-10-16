f63efaf89c21ecf40742c7cf0bce90b608d42ff6
Eduardo <eduardorhsiete@gmail.com>
2024-10-16 19:39:58 +0200

    implemented class Monster

---
+++ irrgarten/monster.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+module Irrgarten
+	class Monster
+		INITIAL_HEALTH = 5
+		@uninitialized_pos = -1
+		def initialize(name, intelligence, strength)
+			@name = name
+			@intelligence = intelligence
+			@strength = strength
+			@health = INITIAL_HEALTH
+			@row, @col = @uninitialized_pos, @uninitialized_pos
+		end
+		
+		def uninitialized_pos
+			uninitialized_pos
+		end
+		
+		def dead
+			return (@health == 0)
+		end
+		
+		def attack
+			Dice.intensity(@strength)
+		end
+		
+		def defend(received_attack)
+			#to be completed later
+		end
+		
+		def set_pos(row, col)
+			@row, @col = row, col
+		end
+		
+		def to_s
+			"Monster #{@name} (health: #{@health}, intelligence: #{@intelligence}, strength: #{@strength}, pos: (#{@row}, #{@col}))"
+		end
+	end
+end
